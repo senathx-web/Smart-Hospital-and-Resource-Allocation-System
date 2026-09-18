5c2c45f86314886895e10e663f28dc66ef13fe78
senathx <dsenath.web@gmail.com>
2026-09-18 16:58:33 +0530

    Created the frontend of basic welcome menu

--- Hospital_Project/display.c
+++ Hospital_Project/display.c
@@ -1,1 +1,19 @@
 //Displaying menus/welcome massages/ outputs  goes here
+#include <stdio.h>
+
+void welcomeMenu(){
+    printf("-----------------------------------------\n");
+    printf("\t\t WELCOME \t\t\n");
+    printf("-----------------------------------------\n");
+
+    printf("1.Register patient\n");
+    printf("2.Display Bed Availability\n");
+    printf("3.Display specialities Details\n");
+    printf("4.Display wards Details\n");
+    printf("5.Display patient Bill\n");
+    printf("6.Display All patients\n");
+    printf("7.Generate summary Report\n");
+    printf("8.Exit\n");
+
+    printf("-----------------------------------------\n");
+}
